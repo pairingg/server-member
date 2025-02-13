286620ef31497757253252fe986e8dfb576a111e
bbungjin <leejintwo12@naver.com>
2025-02-13 18:38:00 +0900

    feat: SpringSecurity 설정

---
+++ src/main/java/pairing/member/jwt/JwtUtils.java
@@ -0,0 +1,48 @@
+package pairing.member.jwt;
+
+
+import java.util.Date;
+import javax.crypto.SecretKey;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import pairing.member.common.MemberRole;
+
+@Slf4j
+@Component
+public class JwtUtils {
+    private final long expiration;
+    private final SecretKey key;
+
+    // JWT 기본값
+    public JwtUtils(
+            @Value("${jwt.expiration}") long expiration,
+            @Value("${jwt.secret}") String secret) {
+        this.expiration = expiration;
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    // JWT 생성하기
+    public String generateToken(String email, Long memberId) {
+        Date expirationDate = new Date(System.currentTimeMillis() + expiration);
+        return Jwts.builder()
+                .subject(email)
+                .expiration(expirationDate)
+                .claim("userId",memberId)
+                .signWith(key)
+                .compact();
+    }
+
+    // JWT에서 정보 가져오기 -> HEADER, PAYLOAD, VERIFY SIGNATURE
+    public String parseToken(String token){
+        Claims payload = (Claims) Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parse(token)
+                .getPayload();
+        return payload.getSubject();
+    }
+}
